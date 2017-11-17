f0f99d5838b69817bc43e79694f87106ff05407f
Yaniv Ironi <iryaniv@gmail.com>
2017-11-17 23:15:34 +0200

    Added example usage.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import PacketShark.*;
+
+import java.io.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PcapReader reader = new PcapReader();
+        try {
+            List<LibpcapPacket> packets = reader.read("../pcaps/dns.pcap");
+            TopParser parser = new TopParser();
+            for (LibpcapPacket p : packets) {
+                LibpcapPacket a = parser.parse(p);
+                System.out.println(a);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
